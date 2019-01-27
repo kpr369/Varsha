fcc09d36f63e09934dfb4797119d07e3c3e9b7c3
Varsha <Varsha@Varsha-PC>
2019-01-27 22:11:18 +0530

    First Commit

---
+++ Varsha/src/main/java/Test/Varsha/App.java
@@ -0,0 +1,13 @@
+package Test.Varsha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
