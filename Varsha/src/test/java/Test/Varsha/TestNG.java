986a54352d8fe9b2919c8aa8e4d13b9d7714a850
Varsha <Varsha@Varsha-PC>
2019-02-23 17:53:19 +0530

    Saving

--- Varsha/src/test/java/Test/Varsha/TestNG.java
+++ Varsha/src/test/java/Test/Varsha/TestNG.java
@@ -1,10 +1,7 @@
 package Test.Varsha;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestNG {
@@ -13,8 +10,7 @@ public class TestNG {
 	public static WebDriver driver1;
 
 	@Test
-	public void kk()
-	{
+	public void kk() {
 		Assert.assertTrue(true);
 	}
 }
